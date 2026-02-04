67caf4f4b3482936c59e0ba0bd5a3b1eac3640d5
Theo <theoscartonlago@gmail.com>
2026-02-04 17:09:17 +0000

    Adding new folders and first component as tutorial (#4)

---
+++ frontend/components/navBar/navBar.tsx
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet } from "react-native";
+import { styles } from "./styles";
+
+export const NavBar = () => {
+  return (
+    <View>
+      <Text style={styles.container}>NavBar</Text>
+    </View>
+  );
+};
