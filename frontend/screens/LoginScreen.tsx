c5c8b5a6d2ff15ef78e9b14725f4f966d30c35c8
scoutingb <ajskelhorn@gmail.com>
2026-02-26 14:53:11 +0000

    Added basic sign up screen, cleaned code for login screen

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { View, StyleSheet, Image } from 'react-native';
-import { Text, useTheme, TextInput, Button } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
 
-import Logo from '../assets/img/logoNoText.png'
-import { useNavigation } from "expo-router";
+import Logo from '../assets/img/logoNoText.png';
 
 export default function OpeningScreen({ navigation }: any) {
 
@@ -12,18 +11,18 @@ export default function OpeningScreen({ navigation }: any) {
 
     return (
         <>
-        <View style={styles.img}>
-            <Image source={Logo} style={{width:200, resizeMode:"contain"}}></Image>
-        </View>
+            <View style={styles.img}>
+                <Image source={Logo} style={{width:200, resizeMode:"contain"}}></Image>
+            </View>
 
-        <View style={{padding:20}}>
-            <TextInput mode='outlined' label="Email" value={emailText} onChangeText={text => setEmailText(text)} style={styles.textBox} />
-            <TextInput mode='outlined' label="Password" value={passText} onChangeText={text => setPassText(text)} secureTextEntry style={styles.textBox} />
-        </View>
+            <View style={{padding:20}}>
+                <TextInput mode='outlined' label="Email" value={emailText} onChangeText={text => setEmailText(text)} style={styles.textBox} />
+                <TextInput mode='outlined' label="Password" value={passText} onChangeText={text => setPassText(text)} secureTextEntry style={styles.textBox} />
+            </View>
 
-        <View style={{marginHorizontal:100}}>
-            <Button icon="account-outline" mode="contained" onPress={() => navigation.navigate('Home')} style={{borderRadius:40, backgroundColor:'#3D8252'}} labelStyle={{fontSize:20, lineHeight:25}} contentStyle={{marginVertical:10}}>Log In</Button>
-        </View>
+            <View style={{marginHorizontal:100}}>
+                <Button icon="account-outline" mode="contained" onPress={() => navigation.navigate('Home')} style={{borderRadius:40, backgroundColor:'#3D8252'}} labelStyle={{fontSize:20, lineHeight:25}} contentStyle={{marginVertical:10}}>Log In</Button>
+            </View>
         </>
     );
 }
--- frontend/screens/SignUpScreen.tsx
+++ frontend/screens/SignUpScreen.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import { View, StyleSheet, Image } from 'react-native';
-import {  Text, useTheme } from "react-native-paper";
+import { TextInput, Button } from "react-native-paper";
 
-export default function OpeningScreen() {
+import Logo from '../assets/img/logoNoText.png';
+
+export default function OpeningScreen({ navigation }: any) {
+
+    const [firstNameText, setFirstNameText] = React.useState("");
+    const [lastNameText, setLastNameText] = React.useState("");
+    const [emailText, setEmailText] = React.useState("");
+    const [passText, setPassText] = React.useState("");
+
     return (
-        <Text variant="displaySmall" style={styles.title}> {/* This is how u put custom colors. U can put MD3 colors in Styleshet*/}
-        Placeholder...
-        </Text>
+        <>
+            <View style={styles.img}>
+                <Image source={Logo} style={{width:200, resizeMode:"contain"}}></Image>
+            </View>
+            
+            <View style={styles.namesView}>
+                <TextInput mode='outlined' label="First Name" value={firstNameText} onChangeText={text => setFirstNameText(text)} style={styles.nameBox} />
+                <TextInput mode='outlined' label="Last Name" value={lastNameText} onChangeText={text => setLastNameText(text)} style={styles.nameBox} />
+            </View>
+
+            <View style={{padding:20}}>
+                <TextInput mode='outlined' label="Email" value={emailText} onChangeText={text => setEmailText(text)} style={styles.textBox} />
+                <TextInput mode='outlined' label="Password" value={passText} onChangeText={text => setPassText(text)} secureTextEntry style={styles.textBox} />
+            </View>
+
+            <View style={{marginHorizontal:100}}>
+                <Button icon="account-outline" mode="contained" onPress={() => navigation.navigate('Home')} style={{borderRadius:40, backgroundColor:'#3D8252'}} labelStyle={{fontSize:20, lineHeight:25}} contentStyle={{marginVertical:10}}>Sign Up</Button>
+            </View>
+        </>
     );
   }
 
-  const styles = StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent:'center',
@@ -22,5 +46,26 @@ export default function OpeningScreen() {
         marginBottom:20,
         textAlign: "center",
         fontSize:30,
+    },
+
+    img: {
+        alignItems:'center'
+    },
+
+    textBox: {
+        marginBottom:10
+    },
+
+    nameBox: {
+        flexGrow:1,
+        flexShrink:1
+    },
+
+    namesView: {
+        paddingHorizontal:20,
+        flexDirection:'row',
+        flexWrap:'wrap',
+        justifyContent:'space-between',
+        gap:20
     }
 });
