9cf105d2748ccf379a21c28a89571cfbd76e8d29
Theo <theoscartonlago@gmail.com>
2026-03-19 13:58:01 +0000

    Inputting ID in all routes, along with finishing conversation controller (still in future need of validations) and began conversation DB services.

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -4,6 +4,8 @@ const router = express.Router(); // Creating a different router for every route,
 // TASK POST (no need for conversation post since if a task is created so is a convo)
 router.post("/" /* TODO */);
 
+router.get("/:id" /*TODO*/);
+
 // TASK PUT
 router.put("/" /* TODO */);
 
--- backend/src/services/conversations.ts
+++ backend/src/services/conversations.ts
@@ -1,22 +1,36 @@
 import { db } from "../db";
 import { JsonObject } from "../generated/prisma/internal/prismaNamespace";
+// CONVERSATIONS SHOULD NOT BE CREATED OR DELETED SINCE TASK AUTO CREATES AND DELETES!
 
 export async function getAllConversations() {
     //This will get all conversations
-    return db.conversation.findMany();
+    return await db.conversation.findMany();
 }
 
-//TODO get conversationById
+export async function getConversationByID(conversationID: number) {
+    return await db.conversation.findUnique({
+        where: { conversationID: conversationID },
+    });
+}
 
-// CONVERSATIONS SHOULD NOT BE CREATED SINCE TASK AUTO CREATES ONE!
-
-export async function createMessage(body: JsonObject) {
+export async function createMessage(conversationID: Number, body: JsonObject) {
     // Should this have their seperate folder? messages? idk
     //TODO create
 }
 
-export async function getAllMessages(body: JsonObject) {
+export async function getAllMessages(conversationID: Number) {
     //TODO create (This will be specific to a conversation)
+}
+
+export async function getMessageByID(
+    conversationID: Number,
+    messageID: Number
+) {
+    //TODO
+}
+
+export async function deleteMessage(conversationID: Number, messageID: Number) {
+    //TODO
 }
 
 // I dont think conversations should be putted? like it should not update with manual input.
@@ -27,6 +41,9 @@ export async function getAllMessages(body: JsonObject) {
 
 export const conversationServices = {
     getAllConversations,
-    createMessage, //TODO THIS MIGHT BE WRONG AND SHOULD BE IN SEPERATE FOLDER
+    createMessage,
+    getConversationByID,
     getAllMessages,
+    getMessageByID,
+    deleteMessage,
 };
