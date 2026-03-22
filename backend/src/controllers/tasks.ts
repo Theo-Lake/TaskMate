c779d821be371ea84a87cc4ffdef87382e636463
Theo <theoscartonlago@gmail.com>
2026-03-22 14:06:29 +0000

    Completing conversation Services with some further needed avaliation

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { taskServices } from "../services/tasks";
 
+// TODO Import validation on all endpoints.
+
 //TODO
 
 export const taskController = {
--- backend/src/services/conversations.ts
+++ backend/src/services/conversations.ts
@@ -9,16 +9,33 @@ export async function getAllConversations() {
 
 export async function getConversationByID(conversationID: number) {
     return await db.conversation.findUnique({
-        where: { conversationID: conversationID },
+        where: { conversationID: conversationID }, //TODO why does this one not need to be Number() ??
     });
 }
 
 export async function createMessage(conversationID: Number, body: JsonObject) {
-    // Should this have their seperate folder? messages? idk
+    //TODO {senderID, content} = body  DESTRUCTURE BODY, then use that to create message.
+    let senderID;
+    let content;
+    let message = await db.message.create({
+        data: {
+            conversationID: Number(conversationID),
+            senderID: Number(senderID),
+            content: String(content),
+        },
+    });
+
+    //TODO Do i need to insert this message into the conversation Message list? or can i just use a query to find it via query convoID?
     //TODO create
+    return;
 }
 
 export async function getAllMessages(conversationID: Number) {
+    return await db.message.findMany({
+        where: {
+            conversationID: Number(conversationID), //TODO Why
+        },
+    });
     //TODO create (This will be specific to a conversation)
 }
 
@@ -26,11 +43,25 @@ export async function getMessageByID(
     conversationID: Number,
     messageID: Number
 ) {
-    //TODO
+    return await db.message.findUnique({
+        where: {
+            messageID: Number(messageID),
+            AND: {
+                conversationID: Number(conversationID),
+            },
+        },
+    });
 }
 
+//TODO why does everything need to be Number() if their type is already declared as Number?
+
 export async function deleteMessage(conversationID: Number, messageID: Number) {
-    //TODO
+    db.message.delete({
+        where: {
+            messageID: Number(messageID),
+            AND: { conversationID: Number(conversationID) },
+        },
+    });
 }
 
 // I dont think conversations should be putted? like it should not update with manual input.
