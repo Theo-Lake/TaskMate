eef512e7e7e1ce22a4b6a235bcc04a80787dde5e
Theo <theoscartonlago@gmail.com>
2026-03-26 11:18:36 +0000

    finished task controller, rout and services

--- backend/src/services/conversations.ts
+++ backend/src/services/conversations.ts
@@ -7,34 +7,31 @@ export async function getAllConversations() {
     return await db.conversation.findMany();
 }
 
-export async function getConversationByID(conversationID: number) {
+export async function getConversationByID(conversationID: Number) {
     return await db.conversation.findUnique({
-        where: { conversationID: conversationID }, //TODO why does this one not need to be Number() ??
+        where: { conversationID: Number(conversationID) },
     });
 }
 
 export async function createMessage(conversationID: Number, body: JsonObject) {
-    let { senderID, content } = body;
-    let message = await db.message.create({
+    const { senderID, content } = body;
+    if (!senderID || !content)
+        throw new Error("senderID or content is missing");
+    return await db.message.create({
         data: {
             conversationID: Number(conversationID),
             senderID: Number(senderID),
             content: String(content),
         },
     });
-
-    //TODO Do i need to insert this message into the conversation Message list? or can i just use a query to find it via query convoID?
-    //TODO create
-    return;
 }
 
 export async function getAllMessages(conversationID: Number) {
     return await db.message.findMany({
         where: {
-            conversationID: Number(conversationID), //TODO Why
+            conversationID: Number(conversationID),
         },
     });
-    //TODO create (This will be specific to a conversation)
 }
 
 export async function getMessageByID(
@@ -51,23 +48,18 @@ export async function getMessageByID(
     });
 }
 
-//TODO why does everything need to be Number() if their type is already declared as Number?
-
 export async function deleteMessage(conversationID: Number, messageID: Number) {
-    await db.message.delete({
+    return await db.message.delete({
         where: {
             messageID: Number(messageID),
             AND: { conversationID: Number(conversationID) },
         },
     });
-    return;
 }
 
 // I dont think conversations should be putted? like it should not update with manual input.
 
-// Is a delete needed? i dont think so since it cascades with the deletion of the task.
-
-// editing a message, and deleting a message should be implemented way later i think.
+// editing a message should be implemented way later i think.
 
 export const conversationServices = {
     getAllConversations,
