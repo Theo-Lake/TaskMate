cdfa5bba3736ce697239560c44dd761674eb74c7
scoutingb <ajskelhorn@gmail.com>
2026-03-29 16:42:29 +0100

    Added card components for task list and noticeboard, created tasks screen.

---
+++ frontend/components/cards/TaskCard.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { Card, Text, useTheme, Avatar } from 'react-native-paper';
+import { ImageSourcePropType } from 'react-native';
+
+// could also add tags for filter?
+type TaskCardProps = {
+  // need avatar to link to user profile
+  title: string;
+  price: string;
+  imageUrl: ImageSourcePropType;
+  // uri image from server
+  // imageUrl: string;
+  description?: string;
+  onPress?: () => void;
+};
+
+export default function TaskCard({
+  title,
+  price,
+  imageUrl,
+  description,
+  onPress,
+}: TaskCardProps) {
+  const theme = useTheme();
+
+  return (
+    <Card mode="outlined" style={styles.card} onPress={onPress}>
+      <View style={styles.row}>
+
+        <Avatar.Icon size={50} icon="account" style={{backgroundColor:'#64A376', marginRight:10}} color="black"/>
+
+        <View style={styles.content}>
+
+          <Text variant="titleSmall" numberOfLines={1} style={{fontWeight: 'bold'}}>
+            {title}
+          </Text>
+          
+          {description ? (
+            <Text
+              variant="bodySmall"
+              numberOfLines={2}
+              style={[styles.description, { color: theme.colors.onSurfaceVariant }]}
+            >
+              {description}
+            </Text>
+          ) : null}
+          
+        </View>
+
+        <Text variant="titleMedium" style={styles.price}>
+            {price}
+        </Text>
+
+        {/* use uri for server hosted images */}
+        {/* <Image source={{ uri: imageUrl }} style={styles.image} /> */}
+
+        <Image source={imageUrl} style={styles.image} />
+      
+      </View>
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginHorizontal: 12,
+    marginVertical: 6,
+    borderRadius: 10,
+    overflow: 'hidden',
+    borderColor: '#C5C5C5',
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: '#E8E8E8',
+  },
+  image: {
+    width: 72,
+    height: 72,
+    borderRadius: 5,
+    marginLeft: 5,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  description: {
+    marginTop: 4,
+  },
+  price: {
+    // probably a better way to do this than a huge top margin
+    marginTop: 40,
+    marginRight: 10,
+    marginLeft:10,
+    fontWeight: 'bold',
+  },
+});
