1ec3c4bf93e601f65ac22774af7cd4149b195bb1
Theo <theoscartonlago@gmail.com>
2026-03-29 22:10:51 +0100

    moved TODO

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -9,9 +9,7 @@ export async function getAllUsers(req: Request, res: Response) {
         console.log("Users GET all accepted.");
         res.status(200).json({ users: users });
     } catch (error) {
-        console.log(
-            `An error occured while trying to get User data: ${error}`
-        );
+        console.log(`An error occured while trying to get User data: ${error}`);
         res.status(500).json({ error: { error } });
     }
 }
@@ -19,35 +17,30 @@ export async function getAllUsers(req: Request, res: Response) {
 // Get user by id function
 export async function getUserById(req: Request, res: Response) {
     try {
-        const userID = Number(req.params.userId)
+        const userID = Number(req.params.userId);
         const user = await userServices.getUserById(userID);
 
         if (!user) {
             res.status(404).json({ error: "User not found" });
             return;
         }
-
         console.log("User by id GET accepted.");
         res.status(200).json({ users: { user } });
     } catch (error) {
-        console.log(
-            `An error occured while trying to get User data: ${error}`
-        );
+        console.log(`An error occured while trying to get User data: ${error}`);
         res.status(500).json({ error: { error } });
     }
 }
 
-
 //TODO include ID in all id needed requests by fetching it from backend URL and make all services take id as input too.
 export async function postUser(req: Request, res: Response) {
     try {
+        const userID = Number(req.params.userId);
         await userServices.createUser(req.body); // Calling user service to create user with req.body
         console.log("User data POST accepted.");
         res.status(200).json({ Message: "User data successfully posted" });
     } catch (error) {
-        console.log(
-            `An error occured while posting the user data: ${error}`
-        );
+        console.log(`An error occured while posting the user data: ${error}`);
         res.status(500).json({ error: error });
     }
 }
@@ -55,19 +48,11 @@ export async function postUser(req: Request, res: Response) {
 export async function putUser(req: Request, res: Response) {
     try {
         const userID = Number(req.params.userId);
-        const user = await userServices.updateUser(userID, req.body); // Calling user service to create update with req.body
-
-        if (!user) {
-            res.status(404).json({ error: "User not found" });
-            return;
-        }
-        
+        await userServices.updateUser(userID, req.body); // Calling user service to create update with req.body
         console.log("User data PUT accepted.");
         res.status(200).json({ Message: "User data successfully updated" });
     } catch (error) {
-        console.log(
-            `An error occured while putting the user data: ${error}`
-        );
+        console.log(`An error occured while putting the user data: ${error}`);
         res.status(500).json({ error: error });
     }
 }
@@ -78,9 +63,7 @@ export async function deleteUser(req: Request, res: Response) {
         console.log("User DELETE accepted.");
         res.status(200).json({ Message: `User ${user} successfully deleted` });
     } catch (error) {
-        console.log(
-            `An error occured while deleting the user data: ${error}`
-        );
+        console.log(`An error occured while deleting the user data: ${error}`);
         res.status(500).json({ error: error });
     }
 }
