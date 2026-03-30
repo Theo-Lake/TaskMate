1a3b978531e57838051cfb9b91d2d007e589f1bb
Theo <theoscartonlago@gmail.com>
2026-03-30 21:53:09 +0100

    Began authentication skeleton

--- backend/src/middleware/authentication/auth.ts
+++ backend/src/middleware/authentication/auth.ts
@@ -1,1 +1,35 @@
-// POST /auth/register /auth/login
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+async function generateAccessToken(userID: number): Promise<string> {
+    //promise means that it will eventually return a string since its an async function
+    return jwt.sign({ userID }, JWT_SECRET, { expiresIn: "15m" });
+}
+
+async function generateRefreshToken(userID: number): Promise<string> {
+    //promise means that it will eventually return a string since its an async function
+    return jwt.sign({ userID }, JWT_SECRET, { expiresIn: "30d" });
+}
+
+//TODO create refreshToken table in prisma
+
+async function hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10); // Increase num for more security though slower hashing
+}
+
+async function comparePassword(
+    password: string,
+    hash: string
+): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+}
+
+//TODO create POST for auth in login and refresh
+
+export const auth = {
+    generateAccessToken,
+    hashPassword,
+    comparePassword,
+};
