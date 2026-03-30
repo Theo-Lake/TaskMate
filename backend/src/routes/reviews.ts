edd83c766226c39c3ccdda7d38070a0767989060
t-raisin <tobz0310@gmail.com>
2026-03-30 17:52:54 +0100

    Fixed Users, Reviews, and Hashtags endpoints so they all work and return what they need to.

--- backend/src/routes/reviews.ts
+++ backend/src/routes/reviews.ts
@@ -4,21 +4,21 @@ import { reviewController } from "../controllers/reviews";
 
 
 // REVIEW GET all reviews user has made ENDPOINT
-router.get("/:userId", reviewController.getReviewsMadeByUser);
+router.get("/published/:userId", reviewController.getReviewsMadeByUser);
 
 // REVIEW GET all reviews user has been given ENDPOINT
-router.get("/:userId", reviewController.getReviewsGivenToUser);
+router.get("/received/:userId", reviewController.getReviewsGivenToUser);
 
 // REVIEW GET review by id ENDPOINT
-router.get("/:reviewID", reviewController.getReviewById);
+router.get("/id/:reviewId", reviewController.getReviewById);
 
 // REVIEW POST create review ENDPOINT
-router.post("/:userId/create/:assigneeId", reviewController.createReview);
+router.post("/create/:userId/:assigneeId", reviewController.createReview);
 
 // REVIEW PUT edit review ENDPOINT
-router.put("/:reviewId", reviewController.updateReview)
+router.put("/update/:reviewId", reviewController.updateReview)
 
 // REVIEW DELETE review ENDPOINT
-router.delete("/:reviewId", reviewController.deleteReview)
+router.delete("/delete/:reviewId", reviewController.deleteReview)
 
 export default router;
