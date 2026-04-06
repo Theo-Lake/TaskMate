a54d6b19a2a402cf6ca734bfabb046216c377267
scoutingb <ajskelhorn@gmail.com>
2026-04-06 17:42:33 +0100

    Updated tasks and events screens to match design doc

--- frontend/screens/EventsScreen/EventsScreen.tsx
+++ frontend/screens/EventsScreen/EventsScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet, Image, FlatList, ScrollView } from 'react-native';
+import { View, StyleSheet, Image, FlatList, ScrollView, SectionList } from 'react-native';
 import {  Text, useTheme,Appbar, Avatar, Chip } from "react-native-paper";
 import {styles} from "../TasksScreen/styles"
 import { MaterialCommunityIcons } from "@expo/vector-icons";
@@ -76,75 +76,130 @@ const events = [
   },
 ];
 
+// hardcoded noticeboard events
+const noticeEvents = [
+  {
+    id: '1',
+    title: 'Games night',
+    time: '17:00',
+    date: '20/12/2026',
+    imageUrl: require('../../assets/img/img.png'),
+    description: 'board games night at barkers!',
+  },
+  {
+    id: '2',
+    title: 'Footy at sports centre',
+    time: '17:00',
+    date: '20/12/2026',
+    imageUrl: require('../../assets/img/img.png'),
+    description: 'chill football match, all skill levels',
+  },
+];
+
+const eventSections = [{
+    title: "events",
+    data: events,
+}];
+
 export default function EventsScreen({navigation}:any) {
     return (
         <>
-        <View>
-            <CustomHeader title="Events" navigation={navigation} showProfilePicture={true} />
-        </View>
-
-        <View style={{flex:1}}>
-
-          <View style={{flexDirection: 'row', marginLeft:10}}>
-            <View style={styles.chip}>
-              <Chip mode="outlined" onPress={() => console.log('sort pressed')}>Sort</Chip>
+            <View>
+                <CustomHeader title="Events" navigation={navigation} showProfilePicture={true} />
             </View>
-
-            <View style={{borderRightWidth:2, borderRightColor:'#969696', marginTop:8, marginBottom:8}}></View>
-
-            {/* could turn into a component for reuse in events screen */}
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{flexDirection: 'row'}} stickyHeaderIndices={[1]}>
-              <View style={[styles.chip, {marginLeft:10}]}>
-                  <Chip
-                      mode="outlined"
-                      onPress={() => console.log('social pressed')}>
-                      Social
-                  </Chip>
-              </View>
-              <View style={styles.chip}>
-                  <Chip mode="outlined" onPress={() => console.log('tennis pressed')}>
-                      Tennis
-                  </Chip>
-              </View>
-              <View style={styles.chip}>
-                  <Chip  mode="outlined" onPress={() => console.log('Football pressed')}>
-                      Football
-                  </Chip>
-              </View>
-              <View style={styles.chip}>
-                  <Chip  mode="outlined" onPress={() => console.log('pub quiz pressed')}>
-                      Pub quiz
-                  </Chip>
-              </View>
-          </ScrollView>
-          </View>
-          <View style={{flex:1}}>
-            <FlatList
-              data={events}
-              keyExtractor={(item) => item.id}
-              showsVerticalScrollIndicator={false}
-              renderItem={({ item }) => (
-                <EventCard
-                  title={item.title}
-                  time={item.time}
-                  date={item.date}
-                  imageUrl={item.imageUrl}
-                  description={item.description}
-                  onPress={() => console.log('Opened task', item.title)}
+    
+            <View style={{flex:1}}>
+                <View>
+                <SectionList
+                    sections={eventSections}
+                    keyExtractor={(item) => item.id}
+                    showsVerticalScrollIndicator={false}
+                    stickySectionHeadersEnabled={true}
+                    ListHeaderComponent={
+                    <>
+                    <View style={{alignItems:'center', paddingTop:10, }}>
+                        <Text variant='titleSmall'>
+                        Events you might like
+                        </Text>
+                    </View>
+    
+                    <View>
+                        <FlatList
+                        horizontal
+                        showsHorizontalScrollIndicator={false}
+                        data={noticeEvents}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => (
+                            <NoticeCard
+                            title={item.title}
+                            price={item.date}
+                            imageUrl={item.imageUrl}
+                            description={item.description}
+                            onPress={() => console.log('Opened event', item.title)}
+                            />
+                        )}
+                        />
+                    </View>
+                    </>
+                    }
+    
+                    renderSectionHeader={() => 
+                    <View style={{flexDirection: 'row', marginLeft:10, backgroundColor:'#f2f2f2'}}>
+                        <View style={styles.chip}>
+                        <Chip mode="outlined" onPress={() => console.log('sort pressed')}>Sort</Chip>
+                        </View>
+    
+                        <View style={{borderRightWidth:2, borderRightColor:'#969696', marginTop:8, marginBottom:8}}></View>
+    
+                        {/* could turn into a component for reuse in events screen */}
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={{flexDirection: 'row'}} stickyHeaderIndices={[1]}>
+                        <View style={[styles.chip, {marginLeft:10}]}>
+                            <Chip
+                                mode="outlined"
+                                onPress={() => console.log('social pressed')}>
+                                Social
+                            </Chip>
+                        </View>
+                        <View style={styles.chip}>
+                            <Chip mode="outlined" onPress={() => console.log('tennis pressed')}>
+                                Tennis
+                            </Chip>
+                        </View>
+                        <View style={styles.chip}>
+                            <Chip  mode="outlined" onPress={() => console.log('football pressed')}>
+                                Football
+                            </Chip>
+                        </View>
+                        <View style={styles.chip}>
+                            <Chip  mode="outlined" onPress={() => console.log('pub quiz pressed')}>
+                                Pub Quiz
+                            </Chip>
+                        </View>
+                        </ScrollView>
+                    </View>
+                    }
+                    renderItem={({ item }) => (
+                    <EventCard
+                        title={item.title}
+                        time={item.time}
+                        date={item.date}
+                        imageUrl={item.imageUrl}
+                        description={item.description}
+                        onPress={() => console.log('Opened event', item.title)}
+                    />
+                    )}
                 />
-              )}
-            />
-          </View>
-
-          <FAB
-              icon="plus"
-              style={styles.fab}
-              customSize={75}
-              color="white"
-              onPress={() => navigation.navigate('CreateEventScreen')}
-          />
-
-        </View>
+                </View>
+    
+                <FAB
+                    icon="plus"
+                    style={styles.fab}
+                    customSize={75}
+                    color="white"
+                    onPress={() => navigation.navigate('CreateTaskScreen')}
+                />
+    
+            </View>
         </>
 
     );
