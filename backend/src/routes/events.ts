74c472e14603acf56a49c11bc3a86a85a08c6328
t-raisin <tobz0310@gmail.com>
2026-04-08 18:36:18 +0100

    Added application to events.

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -18,3 +18,8 @@ router.delete("/:eventId", eventController.deleteEvent);
 router.patch("/:eventId", auth.withAuth, validate(EventUpdateSchema), eventController.updateEvent);
 
 router.patch("/:eventId", auth.withAuth, eventController.patchEventStatus);
+
+router.post("/:eventId/apply/:userId", auth.withAuth, eventController.applyForEvent);
+router.put("/:eventId/apply/:userId/accept", auth.withAuth, eventController.acceptApplication);
+router.put("/:eventId/apply/:userId/reject", auth.withAuth, eventController.rejectApplication);
+router.delete("/:eventId/apply/:userId", auth.withAuth, eventController.unassignEvent);
