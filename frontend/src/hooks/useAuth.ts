ee3563e050259bc598c7fa399c918b3a405dec6a
Theo <theoscartonlago@gmail.com>
2026-04-16 23:44:17 +0100

    Changes to frontend login to store tokens

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import {useMutation} from '@tanstack/react-query';
 import { verifyEmail } from '../api/auth';
 import client from '../api/client';
+import { storeTokens } from '../auth/auth';
 
 
 export const useVerifyEmail = () =>{
@@ -14,6 +15,8 @@ export const useLogin = () => {
     return useMutation({
         mutationFn: async (credentials) => {
             const res = await client.post("/auth/login", credentials);
+            const { accessToken, refreshToken } = res.data;
+            await storeTokens(accessToken, refreshToken);
             return res.data;
         }
     })
