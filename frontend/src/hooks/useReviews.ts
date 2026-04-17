8c97a29753f6be149ea254424d5d9324435faebb
onyshchu <a.onyshchuk@lancaster.ac.uk>
2026-04-17 23:18:50 +0100

    User profile logic

--- frontend/src/hooks/useReviews.ts
+++ frontend/src/hooks/useReviews.ts
@@ -1,13 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
 import client from "../api/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const useReceivedReviews = (userId : number | null) => {
+//need to format rewiews from string to num becuse everythiong on front is asking for num:
+const ratingToNUm: Record<string, number>={
+    'ONE': 1,
+    'TWO':2,
+    'THREE':3,
+    'FOUR':4,
+    'FIVE':5
+}
+
+export const useMyReceivedReviews = () => {
     return useQuery({
-        queryKey:["profile", userId],
+        queryKey:["myReviews"],
         queryFn: async()=>{
-            const res = await client.get(`/reviews/user/${userId}`);
-            return res.data;
+            const myID = await AsyncStorage.getItem('myID');
+            if (!myID){
+                throw new Error('ID is null. relogin')
+            }
+            const res = await client.get(`/reviews/received/${myID}`);
+            console.log(res.data)
+            const rawRew = res.data.reviews
+            return rawRew.map((review:any) =>({
+                ...review,
+                rating: ratingToNUm[review.rating] || Number(review.rating) || 0
+            }))
         },
-        enabled: !!userId,
     })
 }
