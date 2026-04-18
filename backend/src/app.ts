95c4eef8c3ddce31f1b85391c5f71d4ad9e61796
Theo <theoscartonlago@gmail.com>
2026-04-18 10:22:29 +0100

    Added limit rating to protect from DOS and DDOS attacks

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
 import express, {Request, Response} from "express";
 import routes from "./routes/router"  // Might have an error here because it should be index.js (since node compiles in js not ts)
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: { error: "Too many requests, please try again later." },
+});
+
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
+app.use(limiter);
 
 // Getting health check
 // _req is to acknowledge need of parameter, though not the use of it in this current instance.
