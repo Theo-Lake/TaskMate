cf13648e517714dcd23ddd9b9b203824d7eb8ace
onyshchu <a.onyshchuk@lancaster.ac.uk>
2026-04-22 18:37:28 +0100

    Leave review screen created

--- frontend/src/hooks/useReviews.ts
+++ frontend/src/hooks/useReviews.ts
@@ -1,7 +1,9 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import client from "../api/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import { number } from "zod";
+import { createReview, getAllReviews,getReviewById,getReviewsGivenToUser,getReviewsMadeByUser,getUserRating } from "../api/reviews";
+import { Vibration } from "react-native";
 //need to format rewiews from string to num becuse everythiong on front is asking for num:
 const ratingToNUm: Record<string, number>={
     'ONE': 1,
@@ -19,8 +21,7 @@ export const useMyReceivedReviews = () => {
             if (!userId){
                 throw new Error('ID is null. relogin')
             }
-            const res = await client.get(`/reviews/received/${userId}`);
-            const rawRew = res.data
+            const rawRew = await getReviewsGivenToUser(Number(userId));
             return rawRew.map((review:any) =>({
                 ...review,
                 rating: ratingToNUm[review.rating] || Number(review.rating) || 0
@@ -36,13 +37,13 @@ export const useUserReviews = (userId: string | number | undefined) => {
             if (!userId){
                 throw new Error('ID is null. Try again')
             }
-            const res = await client.get(`/reviews/received/${userId}`);
-            const rawRew = res.data
+            const rawRew = await getReviewsGivenToUser(Number(userId));
             return rawRew.map((review:any) =>({
                 ...review,
                 rating: ratingToNUm[review.rating] || Number(review.rating) || 0
             }))
         },
+        enabled: !!userId,
     })
 }
 
@@ -53,13 +54,25 @@ export const useReview = (reviewId: string) => {
             if (!reviewId){
                 throw new Error('ID is null. Try again')
             }
-            const res = await client.get(`/reviews/${reviewId}`);
-            const rawRew = res.data
+            const rawRew = await getReviewById(Number(reviewId));
             return {
                 ...rawRew,
                 rating: ratingToNUm[rawRew.rating] || 0
             };
 
         },
+        enabled: !!reviewId,
+    })
+}
+
+export const useCreateReview = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async({assigneeId, data}: {assigneeId: number, data: any}) => {
+            return await createReview(Number(assigneeId),data);
+        },
+        onSuccess:(res, variables) => {
+            queryClient.invalidateQueries({queryKey:["userReviews", variables.assigneeId]});
+        }
     })
 }
